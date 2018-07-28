64b428d89a7463bdf6f835d6a9b5991e64883914
Maxime Julian <contact@maxime-julian.com>
2018-07-28 20:19:25 +0100

    Fixes

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,5 +1,8 @@
 import Hero from '../../components/Hero';
+import TitleSection from '../../components/TitleSection';
 import IllustrationSection from '../../components/IllustrationSection';
+import H from '../../components/H';
+import P from '../../components/P';
 
 import style from './style';
 
@@ -10,30 +13,37 @@ const Home = () => (
 	<div>
 		<Hero />
 
-		<IllustrationSection
-			className={style.meetingsPageSection}
-			illustration={meetingsPage}
-			features={[{
-				title: 'Meeting management made easy',
-				text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
-			}, {
-				title: 'Meeting management made easy',
-				text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
-			}]}
-		/>
+		<div class={style.features_container}>
+			<TitleSection
+				title="Loads of features"
+				text="Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size."
+			/>
 
-		<IllustrationSection
-			className={style.roomsPageSection}
-			illustration={roomsPage}
-			features={[{
-				title: 'Meeting management made easy',
-				text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
-			}, {
-				title: 'Meeting management made easy',
-				text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
-			}]}
-			reverse
-		/>
+			<IllustrationSection
+				className={style.meetingsPageSection}
+				illustration={meetingsPage}
+				features={[{
+					title: 'Meeting management made easy',
+					text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
+				}, {
+					title: 'Meeting management made easy',
+					text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
+				}]}
+			/>
+
+			<IllustrationSection
+				className={style.roomsPageSection}
+				illustration={roomsPage}
+				features={[{
+					title: 'Meeting management made easy',
+					text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
+				}, {
+					title: 'Meeting management made easy',
+					text: 'Hermes reduces the time spent in planning meetings, manages your meeting rooms and boosts your team’s productivity regardless of your company size.',
+				}]}
+				reverse
+			/>
+		</div>
 	</div>
 );
 
