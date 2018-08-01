da5850a996bf821f3558cb18b8400b6f777f6403
Maxime Julian <contact@maxime-julian.com>
2018-08-01 13:31:54 +0100

    Added transition on header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,6 @@
+import { Component } from 'preact';
+import classNames from 'classnames';
+
 import Container from '../Container';
 import Link from '../Link';
 import Button from '../Button';
@@ -6,32 +9,54 @@ import style from './style';
 
 const hermesLogo = require('../../assets/images/logo-blue.svg');
 
-const Header = () => (
-	<header class={style.header}>
-		<Container className={style.container}>
-			<div class={style.brand}>
-				<img class={style.logo} src={hermesLogo} alt="Hermes logo" />
+class Header extends Component {
+	state = {
+		isScrolled: false,
+	};
 
-				<h3 class={style.name}>
-					Hermes
-				</h3>
-			</div>
+	onScroll = () => {
+		if (window.scrollY > 80 && !this.state.isScrolled) {
+			this.setState({
+				isScrolled: true,
+			});
+		}
+		else if (window.scrollY < 81 && this.state.isScrolled) {
+			this.setState({
+				isScrolled: false,
+			});
+		}
+	};
 
-			<div class={style.nav_container}>
-				<div class={style.nav}>
-					<Link href="" className={style.link_pricing}>Pricing</Link>
-					<Link href="" className={style.link_sign_in}>Sign in</Link>
+	componentWillMount = () => window.addEventListener('scroll', this.onScroll, false);
+	componentWillUnmount = () => window.removeEventListener('scroll', this.onScroll, false);
 
-					<Button
-						className={style.button}
-						label="Register"
-						onClick={() => {}}
-						skeleton
-					/>
+	render = () => (
+		<header class={classNames(style.header, { [style.header_scrolled]: this.state.isScrolled })}>
+			<Container className={style.container}>
+				<div class={style.brand}>
+					<img class={style.logo} src={hermesLogo} alt="Hermes logo" />
+
+					<h3 class={style.name}>
+						Hermes
+					</h3>
 				</div>
-			</div>
-		</Container>
-	</header>
-);
+
+				<div class={style.nav_container}>
+					<div class={style.nav}>
+						<Link href="" className={style.link_pricing}>Pricing</Link>
+						<Link href="" className={style.link_sign_in}>Sign in</Link>
+
+						<Button
+							className={style.button}
+							label="Register"
+							onClick={() => {}}
+							skeleton
+						/>
+					</div>
+				</div>
+			</Container>
+		</header>
+	);
+}
 
 export default Header;
