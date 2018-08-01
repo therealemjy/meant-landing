d2fcfac901c159129b5ae08dd3971cde0a5381d9
Maxime Julian <contact@maxime-julian.com>
2018-08-01 11:02:40 +0100

    CSS fixes

--- src/components/PlansSection/index.js
+++ src/components/PlansSection/index.js
@@ -9,11 +9,13 @@ import style from './style';
 const PlansSection = ({ className }) => (
 	<Section className={className}>
 		<TitleSection className={style.title} title="Simple and affordable pricing" />
+
 		<div class={style.waved_background} >
-			<svg class={style.hero_wave} xmlns="http://www.w3.org/2000/svg" style="background:#fff" viewBox="0 0 1440 112" enable-background="new 0 0 1440 112">
+			<svg class={style.wave} xmlns="http://www.w3.org/2000/svg" style="background:#fff" viewBox="0 0 1440 112" enable-background="new 0 0 1440 112">
 				<path fill="#F2F6FA" fill-rule="nonzero" d="M720 50.8063568c-155.55336-67.7418088-395.55336-67.7418088-720 0V112h1440V50.8063568c-324.44664 67.74180996-564.44664 67.74180996-720 0z" />
 			</svg>
 		</div>
+
 		<Container className={style.container}>
 			<Plan
 				className={style.plan}
