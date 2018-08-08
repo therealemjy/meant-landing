a279ecfec7bf7b850d0a19ade582be962663866d
Maxime Julian <contact@maxime-julian.com>
2018-08-08 12:10:06 +0100

    Fixed typo

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -41,7 +41,7 @@ const Home = () => (
 				illustration={roomsPage}
 				features={[{
 					title: 'Scale',
-					text: 'Expend your workspace as your team grows. Hermes manages meeting rooms out-of-the-box.',
+					text: 'Expand your workspace as your team grows. Hermes manages meeting rooms out-of-the-box.',
 				}, {
 					title: 'Browse',
 					text: 'Find and reserve rooms based on your needs and availabilities, in real-time.',
