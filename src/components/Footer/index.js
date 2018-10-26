67204007eca9037e061be1ac5571d78d73366926
Maxime Julian <contact@maxime-julian.com>
2018-10-27 00:09:44 +0100

    Minor fixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,18 +11,38 @@ const Footer = () => (
 			<div class={[style.section, style.copyright].join(' ')}>
 				{/* <img class={style.logo} src={timowlLogo} alt="Grey Timowl Logo" /> */}
 				<span class={style.copyright_text}>Copyright &copy; 2018 Timowl</span>
-				<span>Part of <a className={style.link_thefamily} href="https://www.thefamily.co/">TheFamily</a> 🖤</span>
+				<span class={style.copyright_family}>
+					Part of{' '}
+					<a
+						className={style.link_thefamily}
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://www.thefamily.co/"
+					>
+						TheFamily 🖤
+					</a>
+				</span>
 			</div>
 
 			<div class={[style.section, style.links_1].join(' ')}>
-				<Link className={style.link} href="https://www.facebook.com/gettimowl/">Facebook</Link>
-				<Link className={style.link} href="https://twitter.com/MeetHermes">Twitter</Link>
-				<Link className={style.link} href="mailto:hello@timowl.io">Contact us</Link>
+				<Link className={style.link} href="https://www.facebook.com/gettimowl/">
+					Facebook
+				</Link>
+				<Link className={style.link} href="https://twitter.com/MeetHermes">
+					Twitter
+				</Link>
+				<Link className={style.link} href="mailto:hello@timowl.io">
+					Contact us
+				</Link>
 			</div>
 
 			<div class={[style.section, style.links_2].join(' ')}>
-				<Link className={style.link} href="/terms-of-use">Terms of Use</Link>
-				<Link className={style.link} href="/privacy-policy">Privacy Policy</Link>
+				<Link className={style.link} href="/terms-of-use">
+					Terms of Use
+				</Link>
+				<Link className={style.link} href="/privacy-policy">
+					Privacy Policy
+				</Link>
 			</div>
 		</Container>
 	</footer>
